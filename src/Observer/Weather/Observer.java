6aa93240c92fd2081fcbc3a1eae37c35317f3184
ryu <jeungwon28@gmail.com>
2023-11-22 13:34:43 +0900

    ADD: ObserverPattern 직접구현 + Observerable을 통한 구현

---
+++ src/Observer/Weather/Observer.java
@@ -0,0 +1,5 @@
+package Observer.Weather;
+
+public interface Observer {
+    public void update();
+}
---
+++ src/Observer/Weather/Subject.java
@@ -0,0 +1,7 @@
+package Observer.Weather;
+
+public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+}
