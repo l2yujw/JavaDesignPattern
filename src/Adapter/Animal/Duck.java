8cb021fbe5c13123ed98a38a46b7586e4eeb6fb3
ryu <jeungwon28@gmail.com>
2023-11-25 21:35:29 +0900

    ADD: AdapterPattern

---
+++ src/Adapter/Animal/Duck.java
@@ -0,0 +1,6 @@
+package Adapter.Animal;
+
+public interface Duck {
+    public void quack();
+    public void fly();
+}
---
+++ src/Adapter/Animal/Turkey.java
@@ -0,0 +1,6 @@
+package Adapter.Animal;
+
+public interface Turkey {
+    public void gobble();
+    public void fly();
+}
